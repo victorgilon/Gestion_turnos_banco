26dc65cafeb1fef498f5ae982a63231157c086a0
Brayan Solis <brayansolis@unicomfacauca.edu.co>
2026-04-18 18:51:32 -0500

    merge traido de la rama victor y rama eduardo

--- frontend/js/registro.js
+++ frontend/js/registro.js
@@ -4,20 +4,15 @@ const formulario = document.getElementById("registro-formulario");
 
 formulario.addEventListener("submit", async (e) => {
     e.preventDefault();
-    console.log("Submit registro detectado");
 
-    // Obtener datos
     const nombre = document.querySelector('input[name="nombre"]').value.trim();
     const email = document.querySelector('input[name="email"]').value.trim();
     const documento = document.querySelector('input[name="documento"]').value.trim();
     const password = document.querySelector('input[name="password"]').value;
     const confirmar = document.querySelector('input[name="confirmar"]').value;
-    const rol = document.querySelector('select[name="rol"]').value;
 
-    console.log("Datos:", { nombre, email, documento, rol });
-
-
-    if (!nombre || !email || !documento || !password || !confirmar || !rol) {
+    // VALIDACIONES
+    if (!nombre || !email || !documento || !password || !confirmar) {
         alert("Todos los campos son obligatorios");
         return;
     }
@@ -37,11 +32,10 @@ formulario.addEventListener("submit", async (e) => {
         return;
     }
 
-
     try {
-        console.log("Enviando datos al servidor...");
+        console.log("Enviando datos al backend...");
 
-        const respuesta = await fetch("http://localhost:5100/api/autenticacion/registro", {
+        const respuesta = await fetch("http://localhost:3000/api/autenticacion/registro", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -51,29 +45,21 @@ formulario.addEventListener("submit", async (e) => {
                 email: email,
                 password: password,
                 documento: documento,
-                rol: rol
+                // 🚨 NO se envía rol, el backend lo asigna
             }),
         });
 
-        console.log("Respuesta:", respuesta);
-
         const data = await respuesta.json();
-        console.log("JSON:", data);
 
         if (!respuesta.ok) {
             alert(data.message || "Error al registrarse");
             return;
         }
 
-        console.log("Registro exitoso");
-
-        alert("Usuario registrado correctamente ");
-
-       
-        window.location.href = "login.html";
-
+        alert("Usuario registrado correctamente");
+        window.location.href = "../login/login.html";
     } catch (error) {
-        console.error("Error:", error);
+        console.error("Error conexión:", error);
         alert("Error de conexión con el servidor");
     }
 });
